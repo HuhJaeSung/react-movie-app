1508c09cb57b93e5475d49f0967bdae7847f004f
JS <gjwotjd112@naver.com>
2022-02-22 13:52:01 +0900

    Make Movie App with React

---
+++ src/routes/Detail.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+function Detail() {
+  const { id } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState([]);
+  const getMovie = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    setMovie(json.data.movie);
+    setLoading(false);
+  };
+  console.log(movie);
+  useEffect(() => {
+    getMovie();
+  }, []);
+  return (
+    <div>
+      {loading ? (
+        <h1>Detail</h1>
+      ) : (
+        <div>
+          <p></p>
+        </div>
+      )}
+    </div>
+  );
+}
+export default Detail;
